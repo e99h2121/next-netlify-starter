2ac3c6811098e55cd0b3ba68aba338e4926277ab
e99h2121 <yamada.nobuk@gmail.com>
2021-05-05 09:20:54 +0900

    add pages

--- pages/index.js
+++ pages/index.js
@@ -4,48 +4,6 @@ import { Planet } from 'react-kawaii'
 import { Cat } from 'react-kawaii'
 import { Ghost } from 'react-kawaii'
 
-import React from 'react';
-import { useListState, randomId } from '@mantine/hooks';
-import { useMantineTheme, Checkbox } from '@mantine/core';
-
-const initialValues = [
-  { label: 'Receive email notifications', checked: false, key: randomId() },
-  { label: 'Receive sms notifications', checked: false, key: randomId() },
-  { label: 'Receive push notifications', checked: false, key: randomId() },
-];
-
-export function IntermediateCheckbox() {
-  const theme = useMantineTheme();
-  const [values, handlers] = useListState(initialValues);
-  const allChecked = values.every((value) => value.checked);
-  const intermediate = values.some((value) => value.checked) && !allChecked;
-
-  const items = values.map((value, index) => (
-    <Checkbox
-      style={{ marginTop: theme.spacing.xs, marginLeft: 33 }}
-      label={value.label}
-      key={value.key}
-      checked={value.checked}
-      onChange={(event) => handlers.setItemProp(index, 'checked', event.currentTarget.checked)}
-    />
-  ));
-
-  return (
-    <div>
-      <Checkbox
-        checked={allChecked}
-        intermediate={intermediate}
-        label="Receive all notifications"
-        onChange={() =>
-          handlers.setState((current) =>
-            current.map((value) => ({ ...value, checked: !allChecked }))
-          )
-        }
-      />
-      {items}
-    </div>
-  );
-}
 
 export default function Home() {
   return (
@@ -78,6 +36,9 @@ export default function Home() {
           <Ghost size={100} mood="excited" color="#83D1FB" />
           <Ghost size={100} mood="happy" color="#83D1FB" />
           <Ghost size={100} mood="ko" color="#83D1FB" />
+          
+          <a href="./about" className={styles.card}><h3>to about page</h3></a>
+          <a href="./mantine" className={styles.card}><h3>to mantine test page</h3></a>
 
 
           <a href="https://nextjs.org/docs" className={styles.card}>
