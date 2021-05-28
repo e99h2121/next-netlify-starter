8431ec472445fa6c6de4b093cee133eb7745c14a
e99h2121 <yamada.nobuk@gmail.com>
2021-05-29 08:14:02 +0900

    delete

--- pages/simplebar/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import app from './app';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<app />, document.getElementById('root'));
-registerServiceWorker();
